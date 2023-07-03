7eeadc18adc544cdd545e5c79bc982dd6749d50f
Marius Niemenmaa <marius.niemenmaa@outlook.com>
2023-07-03 16:15:18 +0300

    Include nix shell

---
+++ shell.nix
@@ -0,0 +1,20 @@
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rustVersion = "latest";
+  rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+    extensions = [
+      "rust-src" # for rust-analyzer
+    ];
+  };
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    rust
+    rust-analyzer
+    lazygit # Optional
+    pkg-config # For GUI
+    cairo # For GUI
+    gtk4 # For GUI
+  ];
+}
